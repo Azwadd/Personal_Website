b9effb6097f6060d09a28597ea64d151053f93bb
Azwad <azwadshameem@outlook.com>
2021-12-29 01:36:32 -0500

    Navbar Started - 25% Progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Link} from 'react-router-dom'
+import {Container, Row, Col, Navbar, Nav} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <div id={"navbar-top"}> </div>
-    </div>
-  );
+import "./App.css";
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "Azwad Shameem",
+            headerLinks: [
+                { title: "Home", path: '/' },
+                { title: "About Me", path: "/about" },
+                { title: "Experience", path: "/experience" },
+                { title: "Projects", path: "/projects" },
+                { title: "Education", path: "/education" },
+                { title: "Contact Me", path: "/contact" }
+            ],
+            home: {
+                title: "Azwad Shameem",
+                subTitle: "Aspiring Software Developer",
+                text: "Check my info out below"
+            },
+            about: {
+                title: "About Me",
+            },
+            projects: {
+                title: "Let's Talk",
+            }
+        }
+    }
+
+    render() {
+        return (
+            <Router>
+                <Container fluid={true} >
+                    <Navbar className={"border-bottom"}>
+                        <Navbar.Brand>Azwad Shameem</Navbar.Brand>
+                        <Nav className={"navbar, navbar-expand-md, sticky-top"}>
+                            <Link classname={"nav-link"} to={'/'}>Home</Link>
+                            <Link classname={"nav-link"} to={'/about'}>About Me</Link>
+                            <Link classname={"nav-link"} to={'/experience'}>Experience</Link>
+                            <Link classname={"nav-link"} to={'/projects'}>Projects</Link>
+                            <Link classname={"nav-link"} to={'/education'}>Education</Link>
+                            <Link classname={"nav-link"} to={'/contact'}>Contact Me</Link>
+                        </Nav>
+                    </Navbar>
+                </Container>
+            </Router>
+        )
+    }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Navbar from "./components/navbar/navbar";
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-/* App is purely to render the structure ONLY*/
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -13,8 +12,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-/* Renders the navigation bar*/
-ReactDOM.render(<Navbar />, document.getElementById("navbar-top"))
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
